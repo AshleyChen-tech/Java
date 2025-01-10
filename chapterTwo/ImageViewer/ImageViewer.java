a30ce31c2e6bc9513440fa8dd8a29cf583947736
Ashley <791752221@qq.com>
2025-01-10 14:07:17 +0800

    new folders and content

---
+++ chapterTwo/ImageViewer/ImageViewer.java
@@ -0,0 +1,15 @@
+package ImageViewer;
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+
+/**
+ * A program for viewing images.
+ * @version 23.0.1 2025.1.10
+ * @author Ashley Chen
+ */
+public class ImageViewer {
+    public static void main(String[] args) {
+        
+    }
+}
